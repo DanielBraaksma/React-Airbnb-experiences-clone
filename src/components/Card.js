71dfe0c0419a7dd2bc4ac75557a1434cced3995d
Daniel Braaksma <danebraaksma@hotmail.com>
2022-03-21 15:44:47 -0400

    added first card

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import Star from "../images/star.png"
+import Katie from "../images/katie-zaferes.png"
+
+export default function Card() {
+    return (
+        <div className="card">
+            <img src={Katie} className="card--image" />
+            <div className="card--stats">
+                <img src={Star} className="card--star" />
+                <span>5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span> / person</p>
+        </div>
+    )
+}
