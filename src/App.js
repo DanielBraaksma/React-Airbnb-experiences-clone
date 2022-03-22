8068c7c360c86d9e8a948762e6728153c18ab871
Daniel Braaksma <danebraaksma@hotmail.com>
2022-03-22 16:29:52 -0400

    added props to card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import Katie from "./images/katie-zaferes.png"
 
-export default function App (){
-    return (
-        <div>
-            <Navbar />
-            <Hero />
-            <Card />
-        </div>
-    )
+export default function App() {
+    // <Hero />
+return (
+<div>
+    <Navbar />
+    <Card
+        img={Katie}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+    />
+</div>
+)
 }
