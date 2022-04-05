acdba789b292c575bfdfa8352a90edd6a80ac5ec
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-05 12:58:01 -0400

    pass the object as a prop instead

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@
 export default function Navbar() {
     return (
         <nav>
-            <img src="./images/logo.png" className="nav--logo"/>
+            <img src="./images/airbnb-logo.png" className="nav--logo"/>
         </nav>
     )
 }
